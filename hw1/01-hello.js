b41675e64e49a9970b55237c753a06d5b57b8a79
Bryan Vazquez <gamer.brybry@gmail.com>
2024-10-12 16:08:26 -0700

    hello, 00-example, 01-paragraphs, and 02-page-sections are completed

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,7 @@
 window.addEventListener('load', loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("My name is Caterina and I will be teaching CS 463/563: Intro to Web evelopment this quarter. Let's get started with the first homework assignment. The first thing we'll do is figure out how to run code checks for accessibility, formatting, and valid HTML and CSS code.");
+
+  let name = 'Bryan';
 }
